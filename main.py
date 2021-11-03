323215e105bfad81efd1a0123162e6fb592221a2
Alfman247 <84677298+Alfman247@users.noreply.github.com>
2021-11-03 16:44:39 +0000

    Version 1.1
    An update to the main code!

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import time
 
+a = 1
+
 print("Welcome to the Chemistry Quiz")
 
 time.sleep(1)
 
-print("How many states of matter are there?")
-a = input("")
-print("There are three, solid liquid and gas")
-time.sleep(1)
-print("What are atoms and compounds?")
-a = input("")
-print("An atom makes up everything else. comounds are a mix of atoms, chemically joined.")
-time.sleep(1)
+
+while a == 0:
+  print("How many states of matter are there?")
+  a = input("")
+  print("There are three, solid liquid and gas")
+  time.sleep(1)
+  print("What are atoms and compounds?")
+  a = input("")
+  print("An atom makes up everything else. comounds are a mix of atoms, chemically joined.")
+  time.sleep(1)
+  print("Name a chemical element! (letters and word)")
+  a = imput("")
+  print("If you want to find out, google for the periodic table to validate your answer")
+  time.sleep(1)
+  print("What can a chemical reaction produce?")
+  a = input("")
+  print("Heat or light!")
+  time.sleep(1)
+  print("Do you want to play again?")
+  a = input("Type 1 for yes and 0 for no")
