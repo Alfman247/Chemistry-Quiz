942cfeb4eddbd00374070f24a5f7eca2dd249565
Alfman247 <84677298+Alfman247@users.noreply.github.com>
2021-11-03 15:56:17 +0000

    Version 1.0
    The basic code!

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+
+print("Welcome to the Chemistry Quiz")
+
+time.sleep(1)
+
+print("How many states of matter are there?")
+a = input("")
+print("There are three, solid liquid and gas")
+time.sleep(1)
+print("What are atoms and compounds?")
+a = input("")
+print("An atom makes up everything else. comounds are a mix of atoms, chemically joined.")
+time.sleep(1)
